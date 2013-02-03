1f0eb8ff11d03334d731b469e5b53342c3a7cf0d
Balazs Ree <ree@greenfinity.hu>
2013-02-03 10:56:29 +0100

    progress tests

--- test/main-test.js
+++ test/main-test.js
@@ -2,6 +2,19 @@ buster.testCase("quipper", {
 
     "is awesome": function () {
         assert(true);
+    },
+
+    "model": {
+
+        "Quip": function() {
+            var quip;
+            quip = App.Quip.create({user: 'User', text: 'Text'});
+            assert.match(quip, {user: 'User', text: 'Text'});
+            quip = App.Quip.create({});
+            assert.match(quip, {user: '', text: ''});
+        }
+
     }
-    
+
+
 });
